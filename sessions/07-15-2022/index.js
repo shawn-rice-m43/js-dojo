f5b31fff7584276c6a003f48364295abce10d962
Shawn Rice <shawn.rice@mark43.com>
2022-07-15 13:59:14 -0400

    07 -15-2022 session results

--- sessions/07-15-2022/index.js
+++ sessions/07-15-2022/index.js
@@ -1,3 +1,40 @@
+/**
+ * Returns true or false depending whether or not a passed string has
+ * entirely unique characters
+ * hasUniqueCharacter('Oswaldo'): false
+ * hasUniqueCharaters('Bay'): true
+ * The function does not care about the case of the characters
+ * @param {string} string
+ */
 export const hasUniqueCharacters = (string) => {
   // do something
+  if (string.length <= 1) {
+    return true;
+  }
+
+  const map = {};
+
+  for (let i = 0; i < string.length; i++) {
+    const character = string[i];
+
+    console.log(character.codePointAt(0));
+    console.log("map", map);
+
+    if (map[character.codePointAt(0)]) {
+      console.log(
+        "map[character.codePointAt(0)]",
+        map[character.codePointAt(0)]
+      );
+
+      return false;
+    } else {
+      map[character.codePointAt(0)] = character.codePointAt(0);
+    }
+  }
+
+  return true;
+
+  // console.log(`${😀.toLocaleString()}`);
+
+  // console.log(map);
 };
--- sessions/07-15-2022/index.test.js
+++ sessions/07-15-2022/index.test.js
@@ -2,6 +2,19 @@ import { hasUniqueCharacters } from "./";
 
 describe("hasUniqueCharacters", () => {
   test("do something", () => {
-    expect(true).toBe(false);
+    expect(hasUniqueCharacters("")).toBe(true);
+    expect(hasUniqueCharacters("0")).toBe(true);
+    expect(hasUniqueCharacters("01")).toBe(true);
+    expect(hasUniqueCharacters("0 1")).toBe(true);
+    expect(hasUniqueCharacters("01234567")).toBe(true);
+    expect(hasUniqueCharacters("😀 😃")).toBe(true);
+
+    expect(hasUniqueCharacters("    ")).toBe(false);
+    expect(hasUniqueCharacters("00")).toBe(false);
+    expect(hasUniqueCharacters("000")).toBe(false);
+    expect(hasUniqueCharacters("010")).toBe(false);
+    expect(hasUniqueCharacters("012345670")).toBe(false);
+    expect(hasUniqueCharacters("0 1 ")).toBe(false);
+    expect(hasUniqueCharacters("😀😀")).toBe(true);
   });
 });
