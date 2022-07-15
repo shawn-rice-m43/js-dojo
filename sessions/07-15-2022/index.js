75a2083cc63ada36cf28cbb31ff3e4095ea5ae9c
Shawn Rice <shawn.rice@mark43.com>
2022-07-14 21:07:24 -0400

    add session for 07-15-2022

---
+++ sessions/07-15-2022/index.js
@@ -0,0 +1,3 @@
+export const hasUniqueCharacters = (string) => {
+  // do something
+};
---
+++ sessions/07-15-2022/index.test.js
@@ -0,0 +1,7 @@
+import { hasUniqueCharacters } from "./";
+
+describe("hasUniqueCharacters", () => {
+  test("do something", () => {
+    expect(true).toBe(false);
+  });
+});
